79599d05691b04b7d27753b5c177f8eba6061c65
rebown <kiril270405@gmail.com>
2025-04-14 22:39:51 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: 'Hello World!',
+    })
+  );
+});
+
+const PORT = 8001;
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
